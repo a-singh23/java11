40f9a50657803ecd265e256d7ed6fcbfb4f04697
deepa <deepasinha39@gmail.com>
2022-06-14 13:55:52 +0530

    Initial commit

---
+++ src/main/java/org/dash/App.java
@@ -0,0 +1,15 @@
+package org.dash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
